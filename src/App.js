1a3cad14e3f2aac86cd68f896111de3370b8f097
Adina <adinaone04@gmail.com>
2024-02-18 14:16:45 +0600

    Added useEffect()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddTaskForm from "./components/AddTaskForm";
 import TaskList from "./components/TaskList";
@@ -6,48 +6,35 @@ import { v4 as uuidv4 } from "uuid";
 import { deleteFuncContext } from "./Context";
 
 function App() {
-  // const tasks = [
-  //   { id: 1, title: "Make a coffee", completed: false },
-  //   { id: 2, title: "GYM for 1.5 hour", completed: false },
-  //   { id: 3, title: "Make a breakfast", completed: false },
-  // ];
-
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState([]);
-  // const [tasksList, setTasksList] = useState([]);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    setItems(items);
+  }, []);
 
   const addItem = () => {
     if (!inputValue) {
       alert("Enter an item");
       return;
     }
-
     const item = {
       id: uuidv4(),
       value: inputValue,
     };
-
     setItems((oldList) => [...oldList, item]);
     console.log(item);
 
     setInputValue("");
   };
-
-  // const addTask = () => {
-  //   if (inputValue === "") {
-  //     return;
-  //   } else {
-  //     setTasksList((tasks) => {
-  //       const newTaskList = [
-  //         ...tasks,
-  //         { id: uuidv4(), title: inputValue, completed: false },
-  //       ];
-  //       console.log(newTaskList);
-  //       return newTaskList;
-  //     });
-  //     setInputValue("");
-  //   }
-  // };
 
   const deleteTask = (id) => {
     setItems((prevTasks) => prevTasks.filter((task) => task.id !== id));
